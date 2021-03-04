abb35968c1f2a1e1288ca5dfbe8e7dad91d69b50
brodys <brody.strock@gmail.com>
2021-03-04 12:34:44 -0700

    new repo

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercise_6._4_number_detector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter a number");
+            try
+            {
+                string prompt = Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            while (prompt != "x")
+            {
+                Console.WriteLine("enter a new number");
+            }
+            
+        }
+        
+    }
+}
