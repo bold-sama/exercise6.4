7a4f042bf6892a8e69d5c0b9da558865913640a7
brodys <brody.strock@gmail.com>
2021-03-16 13:32:23 -0600

    removed main code, restart

--- Program.cs
+++ Program.cs
@@ -6,22 +6,7 @@ namespace exercise_6._4_number_detector
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, please enter a number");
-            try
-            {
-                string prompt = Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-            while (prompt != "x")
-            {
-                Console.WriteLine("enter a new number");
-            }
             
         }
-        
     }
 }
